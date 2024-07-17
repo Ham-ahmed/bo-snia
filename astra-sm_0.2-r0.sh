e1d17d7fff92cbb056fd4128024cc2d762c34610
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-07-18 01:12:05 +0300

    Update astra-sm_0.2-r0.sh

--- astra-sm_0.2-r0.sh
+++ astra-sm_0.2-r0.sh
@@ -7,6 +7,20 @@ wait
 opkg install --force-overwrite /tmp/*.ipk
 wait
 rm -f /var/volatile/tmp/astra-sm.ipk
-wait
 sleep 2;
+echo "" 
+echo "" 
+echo "**************************************************************************************************"
+echo "#                   INSTALLED SUCCESSFULLY              #"
+echo "*                       ON - Panel                      *"
+echo "*                Enigma2 restart is required            *"
+echo "*************************************************************************************************"
+echo "               UPLOADED BY  >>>>   HAMDY_AHMED           "
+sleep 4;
+	echo '========================================================================================='
+###########################################                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo "*************************************************************************************************"
+wait
+killall -9 enigma2
 exit 0
